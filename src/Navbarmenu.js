39656fd1bfb1c98fc3bcb02440ae3c45e7dbf886
sanjaysathu <snjsathu@gmail.com>
2019-12-07 05:16:46 +0530

    fifth

--- src/Navbarmenu.js
+++ src/Navbarmenu.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import {Navbar,Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 
 const Navbarmenu = () =>{
     return(
         <div>
         <Navbar collapseOnSelect expand="md" variant="dark">
-            <Navbar.Brand href="/">Fun Food and Frolic</Navbar.Brand>
+            <Link to='/'><Navbar.Brand>Fun Food and Frolic</Navbar.Brand></Link>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="ml-auto">
-                <Nav.Link href="/" className="px-5">HOME</Nav.Link>
-                <Nav.Link href="/" className="px-5">ABOUT</Nav.Link>
-                <Nav.Link href="/" className="px-5">PLANS</Nav.Link>
-                <Nav.Link href="/" className="px-5">CONTACT US</Nav.Link>
+                <Nav.Link href="#" className="px-5">HOME</Nav.Link>
+                <Nav.Link href="#" className="px-5">ABOUT</Nav.Link>
+                <Nav.Link href="#" className="px-5">PLANS</Nav.Link>
+                <Nav.Link href="#" className="px-5">CONTACT US</Nav.Link>
               </Nav>
             </Navbar.Collapse>
         </Navbar>
