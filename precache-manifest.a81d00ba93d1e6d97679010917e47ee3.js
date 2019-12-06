738acae8574c1936bb78d0278103f01f69143d1f
sanjaysathu <snjsathu@gmail.com>
2019-12-07 04:47:11 +0530

    Updates

---
+++ precache-manifest.a81d00ba93d1e6d97679010917e47ee3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cf80e73b9767623b9e0eabf5299d76",
+    "url": "/FoodWebsite/index.html"
+  },
+  {
+    "revision": "349b533bb013d4b5a652",
+    "url": "/FoodWebsite/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "56b672a959f93ed4ab75",
+    "url": "/FoodWebsite/static/css/main.5daa4db8.chunk.css"
+  },
+  {
+    "revision": "349b533bb013d4b5a652",
+    "url": "/FoodWebsite/static/js/2.bf181d70.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/FoodWebsite/static/js/2.bf181d70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56b672a959f93ed4ab75",
+    "url": "/FoodWebsite/static/js/main.743ff00e.chunk.js"
+  },
+  {
+    "revision": "e6eeacb493aeb119c376",
+    "url": "/FoodWebsite/static/js/runtime-main.2467d522.js"
+  },
+  {
+    "revision": "616ab9ff9fd110838c0a19b9e1e80b1e",
+    "url": "/FoodWebsite/static/media/Wall.616ab9ff.jpg"
+  },
+  {
+    "revision": "671ca4c204cf714d97c1ae4ad78c4683",
+    "url": "/FoodWebsite/static/media/burger.671ca4c2.png"
+  },
+  {
+    "revision": "f1dc629779ba564d446aca04a0cd788f",
+    "url": "/FoodWebsite/static/media/fries.f1dc6297.jpg"
+  },
+  {
+    "revision": "dd8c9029a535950c88cf069692240a24",
+    "url": "/FoodWebsite/static/media/pasta.dd8c9029.jpg"
+  },
+  {
+    "revision": "bef466927b259f6662690825f039b22c",
+    "url": "/FoodWebsite/static/media/pizza.bef46692.jpg"
+  },
+  {
+    "revision": "89db6ca2f1d47e7505e254da49d40604",
+    "url": "/FoodWebsite/static/media/sandwich.89db6ca2.jpg"
+  }
+]);
